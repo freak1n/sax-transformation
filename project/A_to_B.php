8884d57a8b5b25d61dc07960190a303e6c91621f
Yavor Mihaylov <yavor.st.m@gmail.com>
2014-01-09 17:54:53 +0200

    add upload file

--- project/A_to_B.php
+++ project/A_to_B.php
@@ -2,6 +2,10 @@
 $title = "Transform A to B";
 require_once 'inc/header.php';
 ?>
-
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<label for="file-input">XML file of Company A:</label><br />
+	<input type="file" name="file-input" /><br />
+	<input type="submit" value="Submit XML file" />
+</form>
 
 <?php require_once 'inc/footer.php'; ?>
--- project/B_to_A.php
+++ project/B_to_A.php
@@ -3,5 +3,4 @@ $title = "Transform B to A";
 require_once 'inc/header.php';
 ?>
 
-
 <?php require_once 'inc/footer.php'; ?>
